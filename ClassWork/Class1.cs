8b5996bc419370f563766a24f027201714f4718d
Nikita Poletaev <97903488+SweeXXX@users.noreply.github.com>
2022-11-25 21:00:39 +0300

    Add files via upload

---
+++ ClassWork/Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public enum Visibale
+    {
+        Yes = 0,
+        No = 1
+    }
+    public class Figure
+    {
+        protected ConsoleColor color;
+        protected Visibale Visibale;
+        protected string figure;
+        public Figure(string figure, ConsoleColor color, Visibale visibale)
+        {
+            this.figure = figure;
+            this.color = color;
+            this.Visibale = visibale;
+        }
+        public Figure(string figure)
+        {
+            this.figure = figure;
+        }
+        public string GetFigure
+        {
+            get { return figure; }
+            set { figure = value; }
+        }
+        public void PrintStart()
+        {
+            //Console.Clear();
+            int i1 = 0; int i2 = 0;
+            Console.CursorLeft = i1;
+            Console.CursorTop = i2;
+            Console.WriteLine(figure);
+        }
+        //public void PrintVertical(int n=10)
+        //{
+        //    Console.Clear();
+        //    int i1 = 0; int i2 = n;
+        //    Console.ForegroundColor = ConsoleColor.Red;
+        //    Console.CursorLeft = i1;
+        //    Console.CursorTop = i2;
+        //    for(int i =0; i<figure.Length-1; i++)
+        //    {
+        //        if($"{figure[i]}{figure[i+1]}" == "\n" )
+        //        {
+        //            Console.Write("\n");
+        //            for(int j = 0; j < i2; j++)
+        //            {
+        //                Console.Write(" ");
+        //            }
+        //        }
+        //        else
+        //        {
+        //            Console.Write(figure[i]);
+        //        }
+        //    }
+        //    Console.Write(figure[figure.Length-1]);
+        //}
+        public void PrintNew(int n1 = 10, int n2 = 0, ConsoleColor s2 = ConsoleColor.White)
+        {
+            try
+            {
+                //Console.Clear();
+                int i1 = n1; int i2 = n2;
+                Console.ForegroundColor = s2;
+                Console.CursorLeft = i1;
+                Console.CursorTop = i2;
+                for (int i = 0; i < figure.Length - 1; i++)
+                {
+                    if ($"{figure[i]}" == "\n")
+                    {
+                        Console.Write("\n");
+                        for (int j = 0; j < i1; j++)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write(figure[i]);
+                    }
+                }
+                Console.Write(figure[figure.Length - 1]);
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+
+    }
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,117 @@
+﻿static class Programm
+{
+    static void Main()
+    {
+
+        var originalString = "BB";
+
+        var ACoder = new ACoder(originalString);
+
+        Console.WriteLine($"Было:{originalString}");
+        Console.WriteLine($"ACoder.E:{ACoder.Encode()}");
+        Console.WriteLine($"ACoder.D: {ACoder.Decode()}");
+
+        var BCoder = new BCoder(originalString);
+
+        Console.WriteLine($"Было:{originalString}");
+        Console.WriteLine($"BCoder.E: {BCoder.Encode()}");
+        Console.WriteLine($"BCoder.D: {BCoder.Decode()}");
+    }
+}
+
+public interface IСoder
+{
+    string Encode();
+    string Decode();
+}
+
+public class ACoder : IСoder
+{
+    private readonly char[] _inputCharArray;
+
+    public ACoder(string inputString)
+    {
+        _inputCharArray = inputString.ToCharArray();
+    }
+
+    public string Decode()
+    {
+        for (int i = 0; i < _inputCharArray.Length; i++)
+        {
+            _inputCharArray[i] = (char)(_inputCharArray[i] - 1);
+        }
+
+        return string.Join("",_inputCharArray);
+    }
+
+    public string Encode()
+    {
+        for (int i = 0; i < _inputCharArray.Length; i++)
+        {
+            _inputCharArray[i] = (char)(_inputCharArray[i] + 1);
+        }
+
+        return string.Join("", _inputCharArray);
+    }
+}
+public class BCoder : IСoder
+{
+    private readonly Dictionary<char, Tuple<int, int>> _letters = new Dictionary<char, Tuple<int, int>>();//  символ, начальный диапазон, конечный 
+
+    private readonly char[] _inputCharArray;
+
+    private const int _rAlphabetLength = 32;
+    private const int _eAlphabetLength = 26;
+
+    public BCoder(string inputString)
+    {
+        _inputCharArray = inputString.ToCharArray();
+
+        for (int i = 0; i < _rAlphabetLength; i++)
+        {
+            _letters.Add((char)(1072 + i), new Tuple<int, int>(1072, 1072 + _rAlphabetLength - 1));
+            _letters.Add((char)(1040 + i), new Tuple<int, int>(1040, 1040 + _rAlphabetLength - 1));
+
+            if (i < _eAlphabetLength)
+            {
+                _letters.Add((char)(97 + i), new Tuple<int, int>(97, 97 + _eAlphabetLength - 1));
+                _letters.Add((char)(65 + i), new Tuple<int, int>(65, 65 + _eAlphabetLength - 1));
+            }
+        }
+    }
+
+    public string Encode()
+    {
+        for (int i = 0; i < _inputCharArray.Length; i++)
+        {
+            if (_letters.ContainsKey(_inputCharArray[i]))
+            {
+                var el = _letters[_inputCharArray[i]];
+                _inputCharArray[i] = (char)(el.Item2 - (_inputCharArray[i] - el.Item1));
+            }
+            else
+            {
+                _inputCharArray[i] = _inputCharArray[i];
+            }
+        }
+        return string.Concat(_inputCharArray);
+    }
+    public string Decode()
+    {
+
+        for (int i = 0; i < _inputCharArray.Length; i++)
+        {
+            if (_letters.ContainsKey(_inputCharArray[i]))
+            {
+                var el = _letters[_inputCharArray[i]];
+                _inputCharArray[i] = (char)(el.Item2 - (_inputCharArray[i] - el.Item1));
+            }
+            else
+            {
+                _inputCharArray[i] = _inputCharArray[i];
+            }
+
+        }
+        return string.Concat(_inputCharArray);
+    }
+}
